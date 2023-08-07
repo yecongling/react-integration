3703585169de96f831b56f89f241fdf3ab9b4a27
ycl <499475142@qq.com>
2023-08-07 12:56:57 +0800

    病历设计页面完善

--- src/component/emr/dataset/MetaData/index.tsx
+++ src/component/emr/dataset/MetaData/index.tsx
@@ -1,5 +1,5 @@
 import React, {useRef} from "react";
-import {Card, Col, Form, Input, InputRef, Layout, Modal, ModalProps, Row} from "antd";
+import {Card, Col, Form, Input, InputRef, Modal, ModalProps, Row, Select} from "antd";
 
 /**
  * 数据元
@@ -15,34 +15,48 @@ const MetaData: React.FC<ModalProps> = (props) => {
           searchRef.current.focus();
         }
       }}>
-        <Layout style={{height: "100%"}}>
-          <Row style={{height: "100%"}}>
-            <Col span={8}>
-              <Card style={{height: "100%"}} bodyStyle={{height: "100%", padding: "10px"}}>
-                <Input.Search ref={searchRef} autoFocus placeholder="通过名称检索" enterButton/>
-              </Card>
-            </Col>
-            <Col span={16}>
-              <Card style={{marginLeft: "6px", height: "100%"}} bodyStyle={{height: "100%", padding: "10px"}}>
-                <Form
-                  form={menuData}
-                  layout="horizontal"
-                  name="basic"
-                  size="middle"
-                  labelCol={{span: 5}}
-                >
-                  <Form.Item name="name" label="名称" rules={[{required: true, message: '请输入数据元名称！'}]}>
-                    <Input placeholder="名称"/>
-                  </Form.Item>
-                  <Form.Item name="metaCode" label="数据元编码"
-                             rules={[{required: true, message: '请输入数据元编码！'}]}>
-                    <Input placeholder="数据元编码"/>
-                  </Form.Item>
-                </Form>
-              </Card>
-            </Col>
-          </Row>
-        </Layout>
+        <Row style={{height: "100%"}}>
+          <Col span={8}>
+            <Card style={{height: "100%"}} bodyStyle={{height: "100%", padding: "10px"}}>
+              <Input.Search ref={searchRef} autoFocus placeholder="通过名称检索" enterButton/>
+            </Card>
+          </Col>
+          <Col span={16}>
+            <Card style={{marginLeft: "6px", height: "100%"}} bodyStyle={{height: "100%", padding: "10px"}}>
+              <Form
+                form={menuData}
+                layout="horizontal"
+                name="basic"
+                size="middle"
+                labelCol={{span: 5}}
+                initialValues={{
+                  type: 1
+                }}
+              >
+                <Form.Item name="name" label="名称" rules={[{required: true, message: '请输入数据元名称！'}]}>
+                  <Input placeholder="名称"/>
+                </Form.Item>
+                <Form.Item name="description" label="说明" rules={[{required: true, message: '请输入数据元说明！'}]}>
+                  <Input placeholder="说明"/>
+                </Form.Item>
+                <Form.Item name="metaCode" label="数据元编码"
+                           rules={[{required: true, message: '请输入数据元编码！'}]}>
+                  <Input placeholder="数据元编码"/>
+                </Form.Item>
+                <Form.Item name="type" label="录入方式">
+                  <Select
+                    options={[
+                      {
+                        label: "输入框", value: 1
+                      }
+                    ]}
+                  >
+                  </Select>
+                </Form.Item>
+              </Form>
+            </Card>
+          </Col>
+        </Row>
       </Modal>
     </>
   )
@@ -52,6 +66,7 @@ MetaData.defaultProps = {
   title: "数据元维护",
   bodyStyle: {
     height: "500px"
-  }
+  },
+  maskClosable: false
 }
 export default MetaData;
