3c5297d641414485161ab19a03fd4f9c12ce4bb0
yeconglin <499475142@qq.com>
2023-08-15 23:34:00 +0800

    111

--- src/router/RouterBeforeEach.tsx
+++ src/router/RouterBeforeEach.tsx
@@ -4,7 +4,6 @@ import React, {useEffect} from 'react'
 
 const RouterBeforeEach: React.FC = () => {
   const navigate = useNavigate()
-  // const location = useLocation()
   useEffect(() => {
     // 需要添加从后台获取路由的方法，因为在刷新的时候，可能路由就会没了
     // const obj = checkRouterAuth(location.pathname)
@@ -12,7 +11,7 @@ const RouterBeforeEach: React.FC = () => {
     // if (obj && obj.auth && blLogin == 'false') {
     //   navigate('/login', {replace: true})
     // } else {
-      navigate("system/menu");
+    navigate("/home");
     // }
   }, [])
   return <Outlet/>
