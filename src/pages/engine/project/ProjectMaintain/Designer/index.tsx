2d303d5b7774640a64979848aadca6a07baaae86
ycl <499475142@qq.com>
2023-08-17 19:44:23 +0800

    修改页面布局

--- src/pages/engine/project/ProjectMaintain/Designer/index.tsx
+++ src/pages/engine/project/ProjectMaintain/Designer/index.tsx
@@ -1,8 +1,8 @@
 import React from "react";
-import {Button, Card, Col, Input, Modal, Row, Tabs, TabsProps, Tag} from "antd";
+import {Button, Card, Col, Input, Modal, Row, Space, Tabs, TabsProps, Tag} from "antd";
 import './index.less';
 import {useLocation, useNavigate} from "react-router-dom";
-import {ClearOutlined, CompressOutlined, FullscreenOutlined, SaveOutlined} from "@ant-design/icons";
+import {ClearOutlined, CompressOutlined, FullscreenOutlined, RollbackOutlined, SaveOutlined} from "@ant-design/icons";
 
 const Designer: React.FC = () => {
   const navigate = useNavigate();
@@ -23,28 +23,28 @@ const Designer: React.FC = () => {
       key: "trigger",
       label: "触发器",
       children: <>触发器</>
+    },
+    {
+      key: "timer",
+      label: "定时器",
+      children: <>触发器</>
     }
   ]
   return (
     <>
       {contextHolder}
-      <Card className="designer" bodyStyle={{height: '100%', padding: '6px'}}>
-        <Row style={{height: '100%'}}>
-          <Col span={4} className="designer-left" style={{padding: '10px'}}>
+      <Row style={{height: '100%'}} gutter={6}>
+        <Col span={4}>
+          <Card style={{height: '100%'}} bodyStyle={{padding: '10px'}}>
             <Input.Search autoFocus placeholder="通过名称检索" enterButton onSearch={() => alert("检索")}/>
-            <Tabs type="card" tabBarStyle={{marginTop: '6px'}} items={items} tabBarGutter={-1}
-                  className="tab-operator"/>
-          </Col>
-          <Col span={20} className="designer-right">
-            <Row className="designer-toolbar">
-              <Col span={2} style={{paddingLeft: '6px'}}>
-                <Button type="primary" title="保存" icon={<SaveOutlined/>} onClick={() => alert("保存")}/>
-                <Button type="primary" title="清空编辑器" danger icon={<ClearOutlined/>} style={{marginLeft: '6px'}}
-                        onClick={() => {
-                          modal.confirm({title: "确认清空编辑器内容吗？", onOk: () => alert("确认清空编辑器")})
-                        }}/>
-              </Col>
-              <Col span={20} style={{textAlign: 'center'}}>
+            <Tabs tabBarStyle={{marginTop: '6px'}} items={items}/>
+          </Card>
+        </Col>
+        <Col span={20}>
+          <Row className="designer-toolbar">
+            <Card style={{height: '44px', width: '100%'}}
+                  bodyStyle={{display: 'flex', height: '100%', alignItems: 'center'}}>
+              <Col span={10} style={{textAlign: 'center'}}>
                 <Tag color="#108ee9" style={{
                   fontSize: '14px',
                   padding: '5px 7px'
@@ -52,22 +52,32 @@ const Designer: React.FC = () => {
                   <CompressOutlined style={{fontSize: '16px'}}/> :
                   <FullscreenOutlined style={{fontSize: '16px'}}/>}</Tag>
               </Col>
-              <Col span={2} style={{textAlign: 'right'}}>
-                <Button type="primary" onClick={() => {
-                  navigate('/engine/project/projectMaintain')
-                }}>返回</Button>
+              <Col span={14} style={{textAlign: 'right'}}>
+                <Space>
+                  <Button type="primary" title="保存" icon={<SaveOutlined/>} onClick={() => alert("保存")}>保存</Button>
+                  <Button type="primary" title="清空编辑器" danger icon={<ClearOutlined/>}
+                          onClick={() => {
+                            modal.confirm({title: "确认清空编辑器内容吗？", onOk: () => alert("确认清空编辑器")})
+                          }}>清空</Button>
+                  <Button type="primary" icon={<RollbackOutlined/>} onClick={() => {
+                    navigate('/engine/project/projectMaintain')
+                  }}>返回</Button>
+                </Space>
               </Col>
-            </Row>
-            <Row className="designer-content">
-              <Col span={24} className="designer-area">
+            </Card>
+          </Row>
+          <Row className="designer-content" style={{height: 'calc(100% - 50px)', marginTop: '6px'}}>
+            <Col span={24} className="designer-area" style={{height: '100%'}}>
+              <Card style={{height: '100%'}} bodyStyle={{height: '100%', padding: '10px'}}>
                 使用apache camel 实现系统见集成
-              </Col>
-            </Row>
+              </Card>
 
-          </Col>
-        </Row>
-      </Card>
+            </Col>
+          </Row>
+
+        </Col>
+      </Row>
     </>
-  )
+  );
 }
 export default Designer;
